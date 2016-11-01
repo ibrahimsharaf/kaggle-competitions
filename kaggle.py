c0b57bcd5bc7435c13cdbf4d694c438a3997a3ef
Ibrahim Sharaf ElDen <ibrahimsharafelden@gmail.com>
2016-11-01 14:47:05 +0200

    Add files via upload

---
+++ kaggle.py
@@ -0,0 +1,198 @@
+import pandas as pd
+import re
+import nltk
+import nltk.data
+import logging
+import time
+import numpy as np
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+from gensim.models import word2vec
+import gensim
+# nltk.download()
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+train = pd.read_csv("C:\Users\QUDSGROUP\Desktop\Kaggle Word2Vec\Datasets\labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
+test = pd.read_csv("C:\Users\QUDSGROUP\Desktop\Kaggle Word2Vec\Datasets\dtestData.tsv", header=0, delimiter="\t", quoting=3)
+unlabeled_train = pd.read_csv("C:\Users\QUDSGROUP\Desktop\Kaggle Word2Vec\Datasets\unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
+
+print "Read %d  labeled train reviews, %d labeled test reviews, "\
+"and %d unlabeled reviews \n" % (train["review"].size, test["review"].size, unlabeled_train["review"].size)
+
+def review_to_wordlist ( raw_review, remove_stopwords = False ):
+    # Function to convert a raw review to a string of words
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(raw_review).get_text()
+    #
+    # 2. Remove non-letters and non-numbers
+    letters_only = re.sub("[^a-zA-Z1-9]", " ", review_text)
+    #
+    # 3. Convert to lower case, split into individual words
+    words = letters_only.lower().split()
+    #
+    # 4. Optionally remove stopwords
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    #
+    # 5. Return a list of words
+    return (words)
+
+# Load the punkt tokenizer
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+# Define a function to split a review into parsed sentences
+def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence):
+            # Otherwise, call review_to_wordlist to get a list of words
+            sentences.append( review_to_wordlist( raw_sentence, remove_stopwords ))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
+
+
+sentences = [] # Intialize an empty list of sentences
+print "Parsing sentences from training set"
+for review in train["review"]:
+    sentences += review_to_sentences(review.decode("utf8"), tokenizer)
+
+print "Parsing sentences from unlabeled set"
+for review in unlabeled_train["review"]:
+    sentences += review_to_sentences(review.decode("utf8"), tokenizer)
+
+
+# Check how many sentences we have in total - should be around 850,000
+print len(sentences)
+print sentences[0]
+print sentences[1]
+
+
+# Set values for Word2Vec parameters
+num_features = 300    # Word vector dimensionality                      
+min_word_count = 40   # Minimum word count                        
+num_workers = 4       # Number of threads to run in parallel
+context = 10          # Context window size                                                                                    
+downsampling = 1e-3   # Downsample setting for frequent words
+
+# Intialize and train the model
+print "Training model..."
+model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
+# model = word2vec.Word2Vec(sentences, workers=num_workers, size=num_features, \
+#     min_count= min_word_count, window= context, sample = downsampling)
+
+# If you don't plan to train the model any further, calling 
+# init_sims will make the model much more memory-efficient.
+model.init_sims(replace=True)
+
+# It can be helpful to create a meaningful model name and 
+# save the model for later use. You can load it later using Word2Vec.load()
+# model_name = "300features_40minwords_10context"
+# model.save(model_name)
+
+
+# Exploring model results
+# print(model.doesnt_match("man woman child kitchen".split()))
+# print(model.doesnt_match("france england germany berlin".split()))
+# print(model.most_similar("man"))
+# print(model.most_similar("queen"))
+# print(model.most_similar("Egypt"))
+# print(model.most_similar("Beautiful"))
+
+# Index2word is a list that contains the names of the words in 
+# the model's vocabulary. Convert it to a set, for speed 
+index2word_set = set(model.index2word)
+# Vector Avergeing
+def makeFeatureVec(words, model, num_features):
+    # Function to average all of the word vectors in a given
+    # paragraph
+    #
+    # Pre-initialize an empty numpy array (for speed)
+    featureVec = np.zeros((num_features,),dtype="float32")
+    #
+    nwords = 0.
+    # Loop over each word in the review and, if it is in the model's
+    # vocaublary, add its feature vector to the total
+    for word in words:
+        if word in index2word_set: 
+            nwords = nwords + 1.
+            featureVec = np.add(featureVec,model[word])
+    # 
+    # Divide the result by the number of words to get the average
+    featureVec = np.divide(featureVec,nwords)
+    return featureVec
+
+
+def getAvgFeatureVecs(reviews, model, num_features):
+    # Given a set of reviews (each one a list of words), calculate 
+    # the average feature vector for each one and return a 2D numpy array 
+    # 
+    # Initialize a counter
+    counter = 0.
+    # 
+    # Preallocate a 2D numpy array, for speed
+    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+    # 
+    # Loop through the reviews
+    for review in reviews:
+       #
+       # Print a status message every 1000th review
+       if counter%1000. == 0.:
+           print "Review %d of %d" % (counter, len(reviews))
+       # 
+       # Call the function (defined above) that makes average feature vectors
+       reviewFeatureVecs[counter] = makeFeatureVec(review, model, \
+           num_features)
+       #
+       # Increment the counter
+       counter = counter + 1.
+    return reviewFeatureVecs
+
+
+
+# Calculate average feature vectors for training and testing sets,
+# using the functions we defined above. Notice that we now use stop word
+# removal.
+
+clean_train_reviews = []
+for review in train["review"]:
+    clean_train_reviews.append( review_to_wordlist( review, \
+        remove_stopwords=True ))
+
+trainDataVecs = getAvgFeatureVecs( clean_train_reviews, model, num_features )
+
+print "Creating average feature vecs for test reviews"
+clean_test_reviews = []
+for review in test["review"]:
+    clean_test_reviews.append( review_to_wordlist( review, \
+        remove_stopwords=True ))
+
+testDataVecs = getAvgFeatureVecs( clean_test_reviews, model, num_features )
+
+
+# Fit a random forest to the training data, using 100 trees
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier( n_estimators = 100 )
+
+print "Fitting a random forest to labeled training data..."
+forest = forest.fit( trainDataVecs, train["sentiment"] )
+
+# Test & extract results 
+result = forest.predict(testDataVecs)
+# Write the test results 
+output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
+output.to_csv( "Word2Vec_AverageVectors_Google.csv", index=False, quoting=3 )
